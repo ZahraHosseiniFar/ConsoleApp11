ca7c23880223a6df7d2cae75e5fa3bba772a7f96
ZahraHosseiniFar <ZAHRA.HOSSEINI.FAR2000@gmail.com>
2022-11-19 23:33:11 +0330

    Initial commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    class Project
+    {
+       public static void Main(string[] args)
+        {
+        Console.WriteLine("enter one number : ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("the square root of " + a + " is : " + f(a));
+        Console.ReadKey();
+        }
+       private static double f(double x)
+    {
+        x = Math.Sqrt(x);
+        return x; 
+    }
+    }//برنامه ای بنویسید ک ریشه دوم عدد را نمایش دهد
+
